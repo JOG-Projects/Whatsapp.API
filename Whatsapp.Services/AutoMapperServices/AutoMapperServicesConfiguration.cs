1a819da36c095f346b02a37bfda31a59294658ee
João Xavier <fastjonh@gmail.com>
2022-08-03 15:34:09 -0300

    configurando automapper document

--- Whatsapp.Services/AutoMapperServices/AutoMapperServicesConfiguration.cs
+++ Whatsapp.Services/AutoMapperServices/AutoMapperServicesConfiguration.cs
@@ -10,6 +10,7 @@ namespace Whatsapp.Services.AutoMapperServices
             CreateMap<MediaMessageVM, ImageMessage>().ConstructUsing(x => new ImageMessage(x.To, x.Link));
             CreateMap<MediaMessageVM, VideoMessage>().ConstructUsing(x => new VideoMessage(x.To, x.Link));
             CreateMap<MediaMessageVM, AudioMessage>().ConstructUsing(x => new AudioMessage(x.To, x.Link));
+            CreateMap<MediaMessageVM, DocumentMessage>().ConstructUsing(x => new DocumentMessage(x.To, x.Link));
         }
     }
 }
