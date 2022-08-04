ef419d067297168c77f51ccb0647c8003ab3a534
João Xavier <fastjonh@gmail.com>
2022-08-04 15:11:38 -0300

    criando endpoint de teste para demonstrar requisito do cliente

---
+++ Whatsapp.API/Endpoints/TestEndpoints/TestEndpoints.cs
@@ -0,0 +1,38 @@
+﻿using Whatsapp.Domain;
+using Whatsapp.Services.Contracts;
+using Whatsapp.Services.RequisitionService;
+using Whatsapp.Services.ViewModels;
+
+namespace Whatsapp.API.Endpoints.TestEndpoints
+{
+    public class TestEndpoints : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+            app.MapPost("sendLinkAndClientData", HandleLinkAndData);
+        }
+
+        private async Task HandleLinkAndData(IMessageServices messageServices, ClientRepository clientRepository, string clientNumber, string youtubeLink)
+        {
+            const string templateName = "variables_test_01";
+
+            var primeiraRequisicao = clientRepository.GetClient(clientNumber).RegisteredRequisitions.First();
+
+            var components = new List<Component>()
+            {
+                new Component("body", new List<Parameter>()
+                {
+                    new Parameter(primeiraRequisicao.ToString(), youtubeLink),
+                }),
+                new Component("button", new List<Parameter>()
+                {
+                    new Parameter("text", youtubeLink),
+                }, "url", "1"),
+            };
+
+            var vm = new TemplateMessageVM(clientNumber, templateName, components);
+
+            await messageServices.SendTemplateMessage(vm);
+        }
+    }
+}
