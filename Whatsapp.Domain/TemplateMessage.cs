a2607ae723903bf6c1b985051ade6c20c267ebc6
João Xavier <fastjonh@gmail.com>
2022-08-04 14:41:27 -0300

    mapeando parametros do template

--- Whatsapp.Domain/TemplateMessage.cs
+++ Whatsapp.Domain/TemplateMessage.cs
@@ -18,8 +18,9 @@ namespace Whatsapp.Domain
 
     public class Template
     {
-        public Template(string name, string templateLanguage)
+        public Template(string name, string templateLanguage, List<Component>? componentes = null)
         {
+            Components = componentes;
             Language = new(templateLanguage);
             Name = name;
         }
@@ -29,6 +30,39 @@ namespace Whatsapp.Domain
 
         [JsonProperty("language")]
         public Language Language { get; set; }
+
+        [JsonProperty("components")]
+        public List<Component>? Components { get; set; }
+
+    }
+    public class Component
+    {
+        public Component(string type, List<Parameter> parameters)
+        {
+            Type = type;
+            Parameters = parameters;
+        }
+
+        [JsonProperty("type")] 
+        public string Type { get; set; } //"body", "header", "footer"
+
+        [JsonProperty("parameters")]
+        public List<Parameter> Parameters { get; set; }
+    }
+
+    public class Parameter
+    {
+        public Parameter(string type, string text)
+        {
+            Type = type;
+            Text = text;
+        }
+
+        [JsonProperty("type")]
+        public string Type { get; set; } //"text", NAO MAPEADO "currency", NAO MAPEADO "date_time", NAO MAPEADO "image"
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
     }
 
     public class Language
